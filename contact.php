a76a9adcde36e6bf1a5919c6a98cc34f6e8d4cd7
Unknown <samnkwan@gmail.com>
2018-02-03 00:48:41 +1300

    Remove footer pinterest, gallery image tweaks

--- contact.php
+++ contact.php
@@ -1,3 +1,66 @@
+<?php
+var_dump($_POST);
+
+   $name    = '';
+   $email   = '';
+   $phone   = '';
+   $message = '';
+
+   $nameErrMessage   = 'Please enter your name';
+   $emailErrMessage  = 'Please enter a valid email address';
+   $phoneErrMessage  = 'Please enter your phone number';
+   $messageErrMessage= 'Please add a message';
+
+
+if(isset($_POST['send'])){
+
+   $name    = $_POST['name'];
+   $email   = $_POST['email'];
+   $phone   = $_POST['phone'];
+   $message = $_POST['message'];
+
+   // do some server side validation
+   $bPassed          = false;
+   $bPassedName      = false;
+   $bPassedPhone     = true;
+   $bPassedEmail     = false;
+   $bPassedMessage   = false;
+
+
+
+   if(strlen($_POST['name'])){
+
+
+   }
+
+   $bPassedName = strlen($_POST['name']) ? true : false;
+   $bPassedEmail =  (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) ? true : false;
+   $bPassedMessage = strlen($_POST['message']) ? true : false;
+
+   $bPassed = ($bPassedName && $bPassedPhone && $bPassedEmail && $bPassedMessage) ? true : false;
+
+
+var_dump($bPassed);
+
+   //  $to = "email@example.com"; // this is your Email address
+   //  $from = $_POST['email']; // this is the sender's Email address
+   //  $first_name = $_POST['first_name'];
+   //  $last_name = $_POST['last_name'];
+   //  $subject = "Form submission";
+   //  $subject2 = "Copy of your form submission";
+   //  $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+   //  $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+    //
+   //  $headers = "From:" . $from;
+   //  $headers2 = "From:" . $to;
+   //  mail($to,$subject,$message,$headers);
+   //  mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+   //  echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
    <head>
@@ -46,22 +109,37 @@
    		<div class="row">
    			<div class="col-sm-6">
                <div class="height-holder">
-                  <form method="post" id="frmMotorhome" role="form" data-toggle="validator" action="" >
+                  <form method="post" id="contact-from" role="form" data-toggle="validator" action="" >
       				<div class="form-group">
-      					<input type="text" value="" maxlength="40" class="form-control" id="name" name="name" placeholder="NAME" >
+      					<input type="text" value="<?php echo $name; ?>" maxlength="40" class="form-control" id="name" name="name" placeholder="NAME" >
+                     <?php
+                        if(isset($bPassedName) && !$bPassedName){
+                           echo '<div class="help-block with-errors">'.$nameErrMessage.'</div>';
+                        }
+                     ?>
+      				</div>
+
+                  <div class="form-group">
+      					<input type="text" value="<?php echo $email; ?>" maxlength="40" class="form-control" id="email" name="email" placeholder="EMAIL" >
+                     <?php
+                        if(isset($bPassedEmail) && !$bPassedEmail){
+                           echo '<div class="help-block with-errors">'.$emailErrMessage.'</div>';
+                        }
+                     ?>
+      				</div>
+
+                  <div class="form-group">
+      					<input type="text" value="<?php echo $phone; ?>" maxlength="40" class="form-control" id="phone" name="phone" placeholder="PHONE" >
       					<div class="help-block with-errors"></div>
       				</div>
       				<div class="form-group">
-      					<input type="text" value="" maxlength="40" class="form-control" id="fname" name="fname" placeholder="EMAIL" >
-      					<div class="help-block with-errors"></div>
-      				</div>
-      				<div class="form-group">
-      					<input type="text" value="" maxlength="40" class="form-control" id="fname" name="fname" placeholder="PHONE" >
-      					<div class="help-block with-errors"></div>
-      				</div>
-      				<div class="form-group">
-      					<textarea id="other" maxlength="1200" value="" name="other" class="form-control" rows="8" placeholder="MESSAGE"></textarea>
-      				</div>
+      					<textarea id="message" maxlength="1200" value="<?php echo $message; ?>" name="message" class="form-control" rows="8" placeholder="MESSAGE"></textarea>
+                     <?php
+                        if(isset($bPassedMessage) && !$bPassedMessage){
+                           echo '<div class="help-block with-errors">'.$messageErrMessage.'</div>';
+                        }
+                     ?>
+               	</div>
       				<div class="form-group">
       					 <div class="minibanner">
       							<div class="minibanner-end">
